371e7947dfa23269f7329a65ad47b98735599021
wilcastellHome <wilmerjaviercastellanos@gmail.com>
2018-02-26 10:11:54 -0400

    Seeders con llaves foráneas

--- database/seeds/BaseSeeder.php
+++ database/seeds/BaseSeeder.php
@@ -1,10 +1,12 @@
 <?php 
 use Faker\Factory as Faker;
 use Faker\Generator;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
 
-abstract class BaseSeeder extends Seeder
-{
+abstract class BaseSeeder extends Seeder {
+    protected static $pool = array();
+
     protected function createMultiple($total, array $customValues = array())
     {
         for ($i = 1; $i <= $total; $i++) {
@@ -15,11 +17,45 @@ abstract class BaseSeeder extends Seeder
     abstract public function getModel();
 
     abstract public function getDummyData(Generator $faker, array $customValues = array());
-    
+
     protected function create(array $customValues = array())
     {
         $values = $this->getDummyData(Faker::create(), $customValues);
         $values = array_merge($values, $customValues);
-        $this->getModel()->create($values);
+        return $this->addToPool($this->getModel()->create($values));
+    }
+
+    protected function createFrom($seeder, array $customValues = array())
+    {
+        $seeder = new $seeder;
+        return $seeder->create($customValues);
+    }
+
+    protected function getRandom($model)
+    {
+        if ( ! $this->collectionExist($model))
+        {
+            throw new Exception("The $model collection does not exist");
+        }
+        return static::$pool[$model]->random();
+    }
+
+    private function addToPool($entity)
+    {
+        $reflection = new ReflectionClass($entity);
+        $class = $reflection->getShortName();
+        
+        if ( ! $this->collectionExist($class))
+        {
+            static::$pool[$class] = new Collection();
+        }
+        static::$pool[$class]->add($entity);
+        return $entity;
+    }
+
+    
+    private function collectionExist($class)
+    {
+        return isset (static::$pool[$class]);
     }
 }
